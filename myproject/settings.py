fcf86f6952b471212e7ce1dae5fb16dacd6d080b
atomrobic <77220996+atomrobic@users.noreply.github.com>
2025-04-12 20:24:21 +0530

    add

--- myproject/settings.py
+++ myproject/settings.py
@@ -131,7 +131,6 @@ DATABASES = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4000",  # React frontend dev server
     "https://seacareerjob.netlify.app",  # Deployed frontend
-    "*",
     "http://localhost:5173/","http://localhost:5173"
 ]
 
